777e62ecb2d4ba107ff0dea49a03cfe355d3350d
Ignatius Ukwuoma <ignatius.ukwuoma@andela.com>
2017-07-11 10:19:13 +0100

    Update insertion

--- insertion.js
+++ insertion.js
@@ -2,16 +2,16 @@ const linearArray = [2, 4, 6, 8, 12, 14, 16];
 
 function insertion(array) {
   const item = 10;
-  const insertPosition = 4;
+  const insertPosition = 5;
   let numberOfElements = 7;
   let j = numberOfElements;
   let n = numberOfElements + 1;
 
   while(j >= insertPosition) {
-    linearArray[j] = linearArray[j-1];
+    linearArray[j] = linearArray[j - 1];
     j -= 1;
   }
-  linearArray[insertPosition] = item;
+  linearArray[insertPosition - 1] = item;
 
   return linearArray;
 }
